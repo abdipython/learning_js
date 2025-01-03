349882dc0402d6d5599e263933ee15e6884ad7cc
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-03 18:56:44 +0300

    i learnt variables

---
+++ foundations_js/variables.js
@@ -0,0 +1,21 @@
+// variables
+// assigning variables  const novalue = 0
+// declaring variables let x = 2
+// backticks and dollar sign with curly brackets  are used in template literals `${}`
+
+let x = 2; //declaring a variable numbers
+var y = 4; //assigning a variable numbers
+let z = x+y;
+
+const noValue = 0;
+const discount = 100;
+let firstName = "thee"; //string
+let lastName = "fort"; //string
+let online = false //boolean
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(`Your fullnames are ${firstName + lastName}` );
+console.log(`Is Francis online?${online}`);
+console.log(`Your total amount is ${discount - z+ noValue}`);
