a3d8b4e7dadc64546d569157fdb2453e949f1c25
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-08 13:50:54 +0300

    functions in js

---
+++ foundations_js/functions.js
@@ -0,0 +1,31 @@
+//creating functions
+// creating functions using parameters and arguments
+
+ function happyBirthday(userName, age){
+    console.log("Happy birthday to u !!");
+    console.log(`Happy birthday dear ${userName}!!`);
+    console.log(`how old are u now ??${age}`);
+    
+}
+happyBirthday("Abdul",23)
+
+//math functions
+//using return after declaring variables  
+
+function add(x,y){
+   return x + y ; 
+}
+function subtract(x,y){
+    return x - y ; 
+ }
+ function multiply(x,y){
+    return x * y ; 
+ }
+ function divide(x,y){
+    return x / y ; 
+ }
+ 
+ console.log(add(2,4));
+ console.log(subtract(2,4));
+ console.log(multiply(2,4));
+ console.log(divide(2,4));
