b66086ebc10d14933dfb314ad41eaef26b8cbe61
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-14 18:21:13 +0300

    i learnt switches

---
+++ foundations_js/switchs.js
@@ -0,0 +1,28 @@
+// switchs in js
+// dice
+
+let dice = 4;
+let roll; 
+switch (dice){
+
+    case 1:
+    roll = "move once";
+    break;
+    case 2:
+    roll = "move twice";
+    break;
+    case 3:
+    roll = "move thrice";
+    break;
+    case 4:
+    roll = "move four times";
+    break;
+    case 5:
+    roll = "move five times";
+    break;
+    case 6:
+    roll = "move six times";
+    break;
+   
+}
+console.log(roll) ;
