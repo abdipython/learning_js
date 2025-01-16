b622e0bff1c8d9ade8297927a2924fa26dd1c548
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-16 19:51:41 +0300

    while loops

---
+++ foundations_js/whileLoops.js
@@ -0,0 +1,16 @@
+//while loops
+let count = 1 ;
+
+while(count <= 10){
+    console.log(count);
+    count++;
+}
+console.log("    ")//spacing
+
+// do while loops
+let i = 0;
+do{
+    console.log(i);
+    i++;
+    
+}while(i < 99);
