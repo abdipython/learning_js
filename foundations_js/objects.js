11c13cc726aee51877b8db4cd385948487e4367b
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-17 23:50:09 +0300

    i learnt objects in js

---
+++ foundations_js/objects.js
@@ -0,0 +1,21 @@
+// objects in javascript 
+// script for Abdul and zwane meeting 
+const person1 ={
+    firstName:"Abdulraman", 
+    secondName:"saumu",
+    age:23 ,
+    sayhello:function(){
+        console.log("hello there zwane!")
+    }
+}
+const person2 ={
+    firstName:"zwane ", 
+    secondName:"cutie",
+    age:21 ,
+    respondToHello:function(){
+        console.log("hello there fort!")
+    }
+}
+
+person1.sayhello();
+person2.respondToHello();
