418797838804528faec8a9e4fbc605bde3e23591
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-21 21:06:01 +0300

    i am doing conditional statements

---
+++ foundations_js/conditionalStatements.js
@@ -0,0 +1,16 @@
+// conditional statements
+//gretest number checker 
+
+let num1 = 10;
+let num2 = 4;
+let num3 = 5;
+
+if(num1 > num2 && num1 > num3){
+    console.log("num1 is the greatest");
+}
+else if(num2 > num3){
+    console.log("num2 is the greatest");
+}
+else{
+    console.log("num3 is greatest");
+}
