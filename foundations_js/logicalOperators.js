0f5a235749dd9dcb2778a9e08912d75157817349
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-21 21:44:13 +0300

    logical operators

---
+++ foundations_js/logicalOperators.js
@@ -0,0 +1,22 @@
+// logical operators 
+// checking temperature using &&
+
+const temp = 2 ;
+if (temp > 0 && temp <= 30){
+    console.log("the weather is good");
+}
+
+else{
+    console.log("weather is bad over 30 degrees ,dont go outside!!");
+}
+
+console.log("           ")
+
+//checking if its sunny or cloudy 
+ let isCloudy = false;
+ if(!isCloudy){
+    console.log("It is sunny go enjoy ")
+ }
+ else{
+    console.log("Theres no sun ,its cloudy")
+ }
