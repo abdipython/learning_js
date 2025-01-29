545882e30754fa086716fe8587cb5f433579928a
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-01-29 23:46:09 +0300

    finally learnt math functions in js

---
+++ foundations_js/math.js
@@ -0,0 +1,35 @@
+//math functions in js
+
+let x = 5.14;
+  
+//x = Math.round(x);
+//x = Math.sqrt(x);
+//x = Math.ceil(x);
+//x = Math.pow(x,2);
+//x = Math.floor(x);
+x = Math.abs(x);
+
+
+
+
+ console.log(x);
+ console.log("  ")//spacing
+
+let y = 5;
+let z = 10;
+let w = 4;
+let i ;
+let minimum;
+let maximum;
+
+minimum = Math.min(y,z,w);
+maximum = Math.max(y,z,w);
+i = Math.PI
+
+console.log(minimum);
+console.log(maximum);
+console.log(i);
+
+
+
+
