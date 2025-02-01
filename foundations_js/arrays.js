5b4a26848dace27859d415165a9b761f44d2ac64
abdulraman saumu <abdulrahmansaumu@gmail.com>
2025-02-01 18:55:17 +0300

    learnt arrays

---
+++ foundations_js/arrays.js
@@ -0,0 +1,30 @@
+// arrays and their functions 
+let fruits = ["pinapple","banana","apple","kiwi","grapes"]
+
+console.log(fruits[0]);
+console.log(fruits[4]);
+console.log("  ")
+
+// pop function
+let cars = ["volvo","ford","maserati","BMW"]
+
+cars.pop();
+console.log(cars);
+
+//push function
+let vehicles = ["sedan","bus","van",]
+
+vehicles.push("truck");
+console.log(vehicles);
+
+//unshift function 
+let vegetables = ["kales","tomatoes","onions","lettuce"]
+
+vegetables.unshift("cabbage");
+console.log(vegetables);
+
+//shift function
+let legumes = ["peas","blackeyed-beans","green-grams","beans"]
+
+legumes.shift();
+console.log(legumes);
